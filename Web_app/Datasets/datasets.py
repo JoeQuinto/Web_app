b1c2b1f1d92f3c3831592f43533639343306dc31
JoeQuinto <49375540+JoeQuinto@users.noreply.github.com>
2022-05-28 23:48:52 -0500

    Web app first version
    Basic structure for the app.

---
+++ Web_app/Datasets/datasets.py
@@ -0,0 +1,6 @@
+import pandas as pd
+#Datasets
+df1 = pd.read_csv('https://plotly.github.io/datasets/country_indicators.csv')
+df2 = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
+
+datasets = [df1,df2]
---
+++ Web_app/app.py
@@ -0,0 +1,134 @@
+from distutils.log import debug
+import dash
+import dash_bootstrap_components as dbc
+from dash import Input, Output, State, dcc, html
+from pages import page1, page2
+import pandas as pd
+
+
+
+
+
+
+app = dash.Dash(
+    external_stylesheets=[dbc.themes.SOLAR],
+    meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"}
+    ], suppress_callback_exceptions=True
+)
+
+# we use the Row and Col components to construct the sidebar header
+# it consists of a title, and a toggle, the latter is hidden on large screens
+sidebar_header = dbc.Row(
+    [
+        dbc.Col(html.H2("Q Data", className="display-4")),
+        dbc.Col(
+            [
+                html.Button(
+                    # use the Bootstrap navbar-toggler classes to style
+                    html.Span(className="navbar-toggler-icon"),
+                    className="navbar-toggler",
+                    # the navbar-toggler classes don't set color
+                    style={
+                        "color": "rgba(0,0,0,.5)",
+                        "border-color": "rgba(0,0,0,.1)",
+                    },
+                    id="navbar-toggle",
+                ),
+                html.Button(
+                    # use the Bootstrap navbar-toggler classes to style
+                    html.Span(className="navbar-toggler-icon"),
+                    className="navbar-toggler",
+                    # the navbar-toggler classes don't set color
+                    style={
+                        "color": "rgba(0,0,0,.5)",
+                        "border-color": "rgba(0,0,0,.1)",
+                    },
+                    id="sidebar-toggle",
+                ),
+            ],
+            # the column containing the toggle will be only as wide as the
+            # toggle, resulting in the toggle being right aligned
+            width="auto",
+            # vertically align the toggle in the center
+            align="center",
+        ),
+    ]
+)
+
+sidebar = html.Div(
+    [
+        sidebar_header,
+        # we wrap the horizontal rule and short blurb in a div that can be
+        # hidden on a small screen
+        html.Div(
+            
+            html.Hr(),
+               
+            
+            
+        ),
+        # use the Collapse component to animate hiding / revealing links
+        dbc.Collapse(
+            dbc.Nav(
+                [
+                    dbc.NavLink("Home", href="/", active="exact"),
+                    dbc.NavLink("Datasets", href="/sample-datasets", active="exact"),
+                    dbc.NavLink("Visualization", href="/sample-visualizations", active="exact"),
+                ],
+                vertical=True,
+                pills=True,
+            ),
+            id="collapse",
+        ),
+    ],
+    id="sidebar",
+)
+
+content = html.Div(id="page-content")
+
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
+
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname == "/":
+        return html.P("Welcome to Q Data, this is an interactive website for learning and applying Data Science")
+    elif pathname == "/sample-datasets":
+        return page1.layout
+    elif pathname == "/sample-visualizations":
+        return page2.layout
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
+
+
+@app.callback(
+    Output("sidebar", "className"),
+    [Input("sidebar-toggle", "n_clicks")],
+    [State("sidebar", "className")],
+)
+def toggle_classname(n, classname):
+    if n and classname == "":
+        return "collapsed"
+    return ""
+
+
+@app.callback(
+    Output("collapse", "is_open"),
+    [Input("navbar-toggle", "n_clicks")],
+    [State("collapse", "is_open")],
+)
+def toggle_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
+
+if __name__ == "__main__":
+    app.run_server(port=8888, debug=True)
