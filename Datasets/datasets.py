41d4aa5987d90602017dd20ec38dcf8a9cfa4d74
JQuinto <jdquinto10@gmail.com>
2023-02-07 16:37:42 -0500

    Page1-update

--- Datasets/datasets.py
+++ Datasets/datasets.py
@@ -2,5 +2,9 @@ import pandas as pd
 #Datasets
 df1 = pd.read_csv('https://plotly.github.io/datasets/country_indicators.csv')
 df2 = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
+df3 = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
-datasets = [df1,df2]
+datasets = [df1,df2,df3]
+dataset_names = ['Country_indicators', 'Iris_dataset', 'GDP_dataset']
+dataset_dict = dict(zip(dataset_names,datasets))
+
--- pages/back-up.py
+++ pages/back-up.py
@@ -1,7 +1,16 @@
+#back up
 from dash import dcc, html, Input, Output, callback, State, ctx, dash_table
-from Datasets.datasets import datasets, dataset_dict, dataset_names
+from Datasets.datasets import datasets, dataset_dict
 
-
+PAGE_SIZE = 5
+operators = [['ge ', '>='],
+             ['le ', '<='],
+             ['lt ', '<'],
+             ['gt ', '>'],
+             ['ne ', '!='],
+             ['eq ', '='],
+             ['contains '],
+             ['datestartswith ']]
 
 
 def generate_table(df, max_rows=10):
@@ -17,7 +26,16 @@ def generate_table(df, max_rows=10):
                     'textAlign' : 'left'
                     },
         style_header={'backgroundColor': 'rgb(10, 30, 40)'},
-        
+        page_current=0, 
+        page_size= PAGE_SIZE,
+        page_action='custom',
+
+        filter_action='custom',
+        filter_query='',
+        sort_action='custom',
+        sort_mode='multi',
+        sort_by=[],
+
 
         tooltip_data= [{
                     column: {'value': str(value), 'type': 'markdown'}
@@ -26,15 +44,37 @@ def generate_table(df, max_rows=10):
         ], tooltip_duration=None
         )
 
-   
+
+    
+def split_filter_part(filter_part):
+    for operator_type in operators:
+        for operator in operator_type:
+            if operator in filter_part:
+                name_part, value_part = filter_part.split(operator, 1)
+                name = name_part[name_part.find('{') +1: name_part.rfind('}')]
+                value_part = value_part.strip()
+                v0= value_part[0]
+                if (v0 == value_part[-1] and v0 in ("'", '"', '`')):
+                    value = value_part[1: -1].replace('\\' + v0, v0)
+                else:
+                    try:
+                        value = float(value_part)
+                    except ValueError:
+                        value = value_part
+
+                return name, operator_type[0].strip(), value
+    return [None] * 3
 
 layout = html.Div([
     html.H3('Sample datasets'),
     html.P("Select a sample dataset"),
     dcc.Dropdown(
-            dataset_names,
-            id='page-1-dropdown', value='Country_indicators'
+            ['Country values dataset', 'Iris dataset', 'GDP dataset'],
+            id='page-1-dropdown', value='Country values dataset'
         ),
+    
+                
+
     html.Div(id='page-1-display-value'),
     html.Br(),
     html.Div([
@@ -71,16 +111,66 @@ layout = html.Div([
     
 ])
 
+"""
 
 @callback(
     Output('selected-dataset', 'children'),
     Output('name-dataset', 'children'),
     Input('page-1-dropdown', 'value'))
 def display_value(value):
-    df_gen = generate_table(dataset_dict[str(value)])
-       
+    if value == 'Country values dataset':
+        df_gen = generate_table(datasets[0])
+        
+    if value == 'Iris dataset':
+        df_gen = generate_table(datasets[1])
+    if value == 'GDP dataset':
+        df_gen = generate_table(datasets[2])
+        
+    else:
+        df_gen = generate_table(datasets[0])
+    
+    
         
     return df_gen, value
+
+"""
+
+@callback(
+    Output('selected-dataset', 'children'),
+    Input('page-1-dropdown', 'value'),
+    Input('selected_dataset', "page_current"),
+    Input('selected_dataset', "page_size"),
+    Input('selected_dataset', 'sort_by'),
+    Input('selected_dataset', 'filter_query')
+)
+def update_table(name, page_current, page_size, sort_by, filter):
+    filtering_expressions = filter.split(' && ')
+    dff = dataset_dict[str(name)]
+    for filter_part in filtering_expressions:
+        col_name, operator, filter_value = split_filter_part(filter_part)
+
+        if operator in ('eq', 'ne', 'lt', 'le', 'gt', 'ge'):
+            dff = dff.loc[getattr(dff[col_name], operator)(filter_value)]
+        elif operator == 'contains':
+            dff = dff.loc[dff[col_name].str.contains(filter_value)]
+        elif operator == 'datestartswith':
+            dff = dff.loc[dff[col_name].str.startswith(filter_value)]
+    
+    if len(sort_by):
+        dff = dff.sort_values(
+            [col['column_id'] for col in sort_by],
+            ascending=[
+                col['direction'] == 'asc'
+                for col in sort_by
+            ],
+            inplace = False
+        )
+
+    page = page_current
+    size = page_size
+    return generate_table(dff.iloc[page * size: (page+1) * size])
+
+
 
 
 
@@ -109,7 +199,7 @@ def nans (n_clicks, name):
             
             return n_nans
 
-"""
+
 @callback(
     Output('dataset-info', 'children'),
     Input('info-button', 'n_clicks'),
@@ -131,5 +221,4 @@ def info (click_info, click_stats, name):
         df = dataset_dict[str(name)]
         result = generate_table(df.describe())
 
-    return result 
-"""
+    return result
